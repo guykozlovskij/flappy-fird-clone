9bb254df4c5a3417ef1b73b1438be9205daf4634
Guy Kozlovskij <gajus.kozlovskij@gmail.com>
2025-07-26 08:02:30 +0100

    pipe movement done

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @export var moving_pipes := preload("res://Scenes/pipes.tscn")
 @export var spawn_interval:= 2.0 # seconds
-@export var min_y: int = 100
-@export var max_y: int = 400
+@export var min_y: int = 1000
+@export var max_y: int = 1500
 @onready var spawn_timer: Timer = $Timer
 
 func _ready() -> void:
@@ -12,6 +12,6 @@ func _ready() -> void:
 
 func _on_timer_timeout() -> void:
 	var pipe_instance = moving_pipes.instantiate()
-	pipe_instance.scale = Vector2(3.0, 3.0)
+	pipe_instance.scale = Vector2(3.5, 3.5)
 	pipe_instance.position = Vector2(get_viewport_rect().size.x + 100, randf_range(min_y, max_y))
 	add_child(pipe_instance)
--- Scripts/pipes.gd
+++ Scripts/pipes.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-@export var speed: float = 100.0
+@export var speed: float = 300.0
 
 # Called every frame
 func _process(delta: float) -> void:
 	position.x -= speed * delta
 	
 	# Checks if pipe went off screen + additional 200 pixels
-	if position.x > get_viewport_rect().size.x - 200:
+	
+	if position.x < - 200:
 		queue_free()	
  
